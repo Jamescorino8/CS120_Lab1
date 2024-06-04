3e29bed0d551d2a5e85a8cadfd4209da01849192
James Corino <122735477+Jamescorino8@users.noreply.github.com>
2024-06-04 15:01:06 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+/**
+ * Lab 1
+ * @author JAMES CORINO, Nicholas Gati
+ * @Purpose The purpose of this program is to have letters A - K in a indented fassion with A-F going outwards and F-K going inwords 
+ * @version 9/8/21
+ */
+class Main {
+  public static void main(String[] args) {
+    System.out.print("A \n B \n  C \n   D \n    E \n     F \n    G \n   H \n  I \n J \nK");
+  }
+}
